67aa6b2c332a5b43cb744ba32a93d9754bac1a92
louisgauvain <louisgauvainpro@gmail.com>
2023-03-28 15:59:48 +0200

    iframe

---
+++ index.js
@@ -0,0 +1,133 @@
+// Portugal
+const btn_pt = document.getElementById('pt');
+console.log(btn_pt);
+const pt = document.getElementById('jqvmap2_pt');
+console.log(pt);
+btn_pt.addEventListener('mouseover', () => {
+	pt.style.fill = '#fdc735';
+	btn_pt.style.color = '#fd9800';
+});
+btn_pt.addEventListener('mouseout', () => {
+	pt.style.fill = '#724099';
+	btn_pt.style.color = '#000000';
+});
+
+// Angleterre
+const btn_gb = document.getElementById('gb');
+const gb = document.getElementById('jqvmap2_en');
+btn_gb.addEventListener('mouseover', () => {
+	gb.style.fill = '#fdc735';
+	btn_gb.style.color = '#fd9800';
+});
+btn_gb.addEventListener('mouseout', () => {
+	gb.style.fill = '#724099';
+	btn_gb.style.color = '#000000';
+});
+
+// Suède
+const btn_se = document.getElementById('se');
+const se = document.getElementById('jqvmap2_se');
+btn_se.addEventListener('mouseover', () => {
+	se.style.fill = '#fdc735';
+	btn_se.style.color = '#fd9800';
+});
+btn_se.addEventListener('mouseout', () => {
+	se.style.fill = '#724099';
+	btn_se.style.color = '#000000';
+});
+
+// Pays-bas
+const btn_nl = document.getElementById('nl');
+const nl = document.getElementById('jqvmap2_nl');
+btn_nl.addEventListener('mouseover', () => {
+	nl.style.fill = '#fdc735';
+	btn_nl.style.color = '#fd9800';
+});
+btn_nl.addEventListener('mouseout', () => {
+	nl.style.fill = '#724099';
+	btn_nl.style.color = '#000000';
+});
+
+// Danemark
+const btn_dk = document.getElementById('dk');
+const dk = document.getElementById('jqvmap2_dk');
+btn_dk.addEventListener('mouseover', () => {
+	dk.style.fill = '#fdc735';
+	btn_dk.style.color = '#fd9800';
+});
+btn_dk.addEventListener('mouseout', () => {
+	dk.style.fill = '#724099';
+	btn_dk.style.color = '#000000';
+});
+
+// Irlande
+const btn_ie = document.getElementById('ie');
+const ie = document.getElementById('jqvmap2_ie');
+btn_ie.addEventListener('mouseover', () => {
+	ie.style.fill = '#fdc735';
+	btn_ie.style.color = '#fd9800';
+});
+btn_ie.addEventListener('mouseout', () => {
+	ie.style.fill = '#724099';
+	btn_ie.style.color = '#000000';
+});
+
+// Ecosse
+const btn_sc = document.getElementById('sc');
+const sc = document.getElementById('jqvmap2_sc');
+btn_sc.addEventListener('mouseover', () => {
+	sc.style.fill = '#fdc735';
+	btn_sc.style.color = '#fd9800';
+});
+btn_sc.addEventListener('mouseout', () => {
+	sc.style.fill = '#724099';
+	btn_sc.style.color = '#000000';
+});
+
+//Belgique
+const btn_be = document.getElementById('be');
+const be = document.getElementById('jqvmap2_be');
+btn_be.addEventListener('mouseover', () => {
+	be.style.fill = '#fdc735';
+	btn_be.style.color = '#fd9800';
+});
+btn_be.addEventListener('mouseout', () => {
+	be.style.fill = '#724099';
+	btn_be.style.color = '#000000';
+});
+
+// France
+const btn_fr = document.getElementById('fr');
+const fr = document.getElementById('jqvmap2_fr');
+btn_fr.addEventListener('mouseover', () => {
+	fr.style.fill = '#fdc735';
+	btn_fr.style.color = '#fd9800';
+});
+btn_fr.addEventListener('mouseout', () => {
+	fr.style.fill = '#724099';
+	btn_fr.style.color = '#000000';
+});
+
+// Allemagne
+const btn_de = document.getElementById('de');
+const de = document.getElementById('jqvmap2_de');
+btn_de.addEventListener('mouseover', () => {
+	de.style.fill = '#fdc735';
+	btn_de.style.color = '#fd9800';
+});
+btn_de.addEventListener('mouseout', () => {
+	de.style.fill = '#724099';
+	btn_de.style.color = '#000000';
+});
+
+// Norvège
+const btn_no = document.getElementById('no');
+const no = document.getElementById('jqvmap2_no');
+btn_no.addEventListener('mouseover', () => {
+	no.style.fill = '#fdc735';
+	btn_no.style.color = '#fd9800';
+});
+btn_no.addEventListener('mouseout', () => {
+	no.style.fill = '#724099';
+	btn_no.style.color = '#000000';
+});
